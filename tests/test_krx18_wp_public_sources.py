4298292cb697458e5f5fede62b91d52bb466a763
aliprogram198i <alihumsyali@gmail.com>
2026-09-15 19:22:52 +0300

    test: tighten KRX18 WordPress source extraction

--- tests/test_krx18_wp_public_sources.py
+++ tests/test_krx18_wp_public_sources.py
@@ -6,6 +6,23 @@ def test_post_id_from_krx18_url():
 
 
 def test_extract_only_explicit_server_targets():
-    html = '<div><span>Video Sources</span><a href="https://playkrx18.site/x">Server 1</a><a href="https://mov18plus.cloud/y">Server 2</a><a href="https://ads.example/">Advertisement</a></div>'
+    html = '''
+    <div><span>Video Sources</span>
+      <a href="https://playkrx18.site/x">Server 1</a>
+      <a href="https://mov18plus.cloud/y">Server 2</a>
+      <a href="https://ads.example/">Advertisement</a>
+      <a href="https://example.invalid/unrelated-watch">Watch unrelated</a>
+    </div>
+    '''
     targets = set(extract_server_targets(html, 'https://krx18.com/movies/84170-test/'))
     assert targets == {'https://playkrx18.site/x', 'https://mov18plus.cloud/y'}
+
+
+def test_plain_url_requires_nearby_server_marker():
+    html = '''
+    <div>Server 1 https://playkrx18.site/watch/84170</div>
+    <div>https://example.invalid/unrelated.mp4</div>
+    '''
+    assert extract_server_targets(html, 'https://krx18.com/movies/84170-test/') == [
+        'https://playkrx18.site/watch/84170'
+    ]
