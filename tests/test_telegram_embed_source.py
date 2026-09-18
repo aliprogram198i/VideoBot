c9ba0be2d9bbff5ec543e2936344291072afca78
aliprogram198i <alihumsyali@gmail.com>
2026-09-18 03:16:32 +0300

    test: cover Telegram embed source variants

---
+++ tests/test_telegram_embed_source.py
@@ -0,0 +1,30 @@
+import unittest
+
+from downloader.smart_media_bridge import _telegram_embed_urls
+
+
+class TelegramEmbedSourceTests(unittest.TestCase):
+    def test_public_post_generates_deterministic_embed_variants(self):
+        urls = _telegram_embed_urls("https://t.me/syrevarch/8453")
+        self.assertEqual(
+            urls,
+            [
+                "https://t.me/syrevarch/8453?embed=1&mode=tme",
+                "https://t.me/syrevarch/8453?embed=1",
+                "https://t.me/s/syrevarch/8453?embed=1&mode=tme",
+                "https://t.me/s/syrevarch/8453?embed=1",
+            ],
+        )
+
+    def test_query_and_fragment_do_not_change_identity(self):
+        self.assertEqual(
+            _telegram_embed_urls("https://t.me/syrevarch/8453?single=1#x")[0],
+            "https://t.me/syrevarch/8453?embed=1&mode=tme",
+        )
+
+    def test_non_post_telegram_url_is_not_resolved(self):
+        self.assertEqual(_telegram_embed_urls("https://t.me/syrevarch"), [])
+
+
+if __name__ == "__main__":
+    unittest.main()
