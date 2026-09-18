00ab5fd0dd1fdc7abd1ae394341397fc6aaedd10
aliprogram198i <alihumsyali@gmail.com>
2026-09-18 07:18:53 +0300

    chore: wire Instagram failure classifier

--- bot.py
+++ bot.py
@@ -40,6 +40,7 @@ from downloader.instagram_identity import (
     parse_instagram_post_url,
 )
 
+from downloader.instagram_failure import instagram_failure_message
 try:
     from google import genai
 except ImportError:
