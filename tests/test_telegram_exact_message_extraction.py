dbab3df476c37d5611307e286c59e7b02afc7da5
aliprogram198i <alihumsyali@gmail.com>
2026-09-18 03:26:18 +0300

    test: isolate exact Telegram message media

---
+++ tests/test_telegram_exact_message_extraction.py
@@ -0,0 +1,56 @@
+import unittest
+
+from downloader.smart_extractor import extract_telegram_post_candidates
+
+
+class TelegramExactMessageExtractionTests(unittest.TestCase):
+    def test_only_requested_message_media_is_extracted(self):
+        page = '''
+        <div class="tgme_widget_message_wrap js-widget_message_wrap">
+          <div class="tgme_widget_message" data-post="syrevarch/8452">
+            <video src="https://cdn.example/previous.mp4"></video>
+            <a class="tgme_widget_message_date" href="https://t.me/syrevarch/8452"></a>
+          </div>
+        </div>
+        <div class="tgme_widget_message_wrap js-widget_message_wrap">
+          <div class="tgme_widget_message" data-post="syrevarch/8453">
+            <video src="https://cdn.example/target.mp4"></video>
+            <a class="tgme_widget_message_date" href="https://t.me/syrevarch/8453"></a>
+          </div>
+        </div>
+        <div class="tgme_widget_message_wrap js-widget_message_wrap">
+          <div class="tgme_widget_message" data-post="syrevarch/8454">
+            <video src="https://cdn.example/next.mp4"></video>
+            <a class="tgme_widget_message_date" href="https://t.me/syrevarch/8454"></a>
+          </div>
+        </div>
+        '''
+        candidates = extract_telegram_post_candidates(
+            page,
+            "https://t.me/syrevarch/8453?embed=1",
+            channel="syrevarch",
+            message_id=8453,
+        )
+        self.assertEqual([item.url for item in candidates], ["https://cdn.example/target.mp4"])
+
+    def test_missing_requested_message_fails_closed(self):
+        page = '''
+        <div class="tgme_widget_message_wrap">
+          <div class="tgme_widget_message" data-post="syrevarch/8454">
+            <video src="https://cdn.example/next.mp4"></video>
+          </div>
+        </div>
+        '''
+        self.assertEqual(
+            extract_telegram_post_candidates(
+                page,
+                "https://t.me/syrevarch/8453?embed=1",
+                channel="syrevarch",
+                message_id=8453,
+            ),
+            [],
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
