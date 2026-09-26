db9dacf22390a339c5dcb6db1ed824132c873773
aliprogram198i <alihumsyali@gmail.com>
2026-09-26 11:02:49 +0300

    test: cover Facebook share/r canonical id recovery

--- tests/test_smart_media_bridge_identity.py
+++ tests/test_smart_media_bridge_identity.py
@@ -1,6 +1,10 @@
 import unittest
 
-from downloader.smart_media_bridge import _facebook_embed_urls, _protected_social_post
+from downloader.smart_media_bridge import (
+    _facebook_embed_urls,
+    _facebook_resolved_id_from_error,
+    _protected_social_post,
+)
 
 
 class SmartMediaBridgeIdentityTests(unittest.TestCase):
@@ -12,13 +16,25 @@ class SmartMediaBridgeIdentityTests(unittest.TestCase):
         self.assertTrue(_protected_social_post("https://www.instagram.com/reel/DdYyj56qw54/?stkn=x"))
         self.assertTrue(_protected_social_post("https://www.instagram.com/p/ABC_123/"))
 
-    def test_facebook_share_r_routes_to_official_embed(self):
+    def test_facebook_share_r_resolves_numeric_id_from_ytdlp_error(self):
         url = "https://www.facebook.com/share/r/1BvGx4dCiQ/"
-        variants = _facebook_embed_urls(url)
-        self.assertEqual(len(variants), 1)
-        self.assertIn("plugins/video.php", variants[0])
-        self.assertIn("1BvGx4dCiQ", variants[0])
-        self.assertIn("href=https%3A%2F%2Fwww.facebook.com%2Fshare%2Fr%2F1BvGx4dCiQ%2F", variants[0])
+        error = (
+            "ERROR: [facebook] 2561442584302940: Cannot parse data; "
+            "please report this issue on https://github.com/yt-dlp/yt-dlp/issues"
+        )
+        resolved_id = _facebook_resolved_id_from_error(url, error)
+        self.assertEqual(resolved_id, "2561442584302940")
+
+        variants = _facebook_embed_urls(
+            url,
+            resolved_video_id=resolved_id,
+        )
+        self.assertEqual(len(variants), 2)
+        self.assertIn(
+            "href=https%3A%2F%2Fwww.facebook.com%2Freel%2F2561442584302940%2F",
+            variants[0],
+        )
+        self.assertIn("2561442584302940", variants[1])
 
     def test_facebook_reel_keeps_numeric_embed_variants(self):
         variants = _facebook_embed_urls("https://www.facebook.com/reel/1474514414731627/")
