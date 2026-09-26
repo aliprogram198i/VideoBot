cf4025574b6cd593aa7c58cd70db05306adb5bc5
aliprogram198i <alihumsyali@gmail.com>
2026-09-26 15:40:58 +0300

    test: cover Facebook share metadata ID resolution

--- tests/test_smart_media_bridge_identity.py
+++ tests/test_smart_media_bridge_identity.py
@@ -65,6 +65,34 @@ class SmartMediaBridgeIdentityTests(unittest.TestCase):
             "2561442584302940",
         )
 
+    def test_facebook_share_r_resolves_explicit_video_id_metadata(self):
+        class FakeResponse:
+            def geturl(self):
+                return "https://www.facebook.com/share/r/1BvGx4dCiQ/"
+
+            def read(self, _limit):
+                return b'{"video_id":"2561442584302940","title":"public reel"}'
+
+            def __enter__(self):
+                return self
+
+            def __exit__(self, *args):
+                return False
+
+        class FakeBot:
+            Request = staticmethod(lambda *args, **kwargs: object())
+
+            @staticmethod
+            def safe_urlopen(*args, **kwargs):
+                return FakeResponse()
+
+        self.assertEqual(
+            _facebook_resolve_share_id(
+                FakeBot,
+                "https://www.facebook.com/share/r/1BvGx4dCiQ/",
+            ),
+            "2561442584302940",
+        )
     def test_facebook_share_r_resolver_rejects_non_facebook_canonical_url(self):
         class FakeResponse:
             def geturl(self):
