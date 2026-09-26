6eb946e10572e9160b093d1bc0f9610cd6cc960f
aliprogram198i <alihumsyali@gmail.com>
2026-09-26 12:11:22 +0300

    test: lock Facebook share canonical ID resolution

--- tests/test_facebook_embed_fallback.py
+++ tests/test_facebook_embed_fallback.py
@@ -22,3 +22,50 @@ def test_facebook_embed_rejects_non_facebook_or_malformed_urls():
     assert _facebook_embed_urls("https://www.youtube.com/watch?v=2115871489331970") == []
     assert _facebook_embed_urls("https://www.facebook.com/reel/not-a-numeric-id/") == []
     assert _facebook_embed_urls("not-a-url") == []
+
+
+from types import SimpleNamespace
+
+from downloader.smart_media_bridge import _facebook_resolve_share_id
+
+
+SHARE_URL = "https://www.facebook.com/share/r/1BvGx4dCiQ/"
+RESOLVED_ID = "2561442584302940"
+
+
+def test_facebook_share_resolves_canonical_id_from_redirect(monkeypatch):
+    class FakeResponse:
+        def __init__(self, final_url):
+            self.final_url = final_url
+
+        def __enter__(self):
+            return self
+
+        def __exit__(self, *args):
+            return False
+
+        def geturl(self):
+            return self.final_url
+
+        def read(self, _limit):
+            return b""
+
+    class FakeBot:
+        Request = staticmethod(lambda url, headers: SimpleNamespace(url=url, headers=headers))
+
+        @staticmethod
+        def safe_urlopen(request, timeout, max_bytes):
+            assert timeout == 12
+            assert max_bytes == 512 * 1024
+            return FakeResponse(
+                "https://www.facebook.com/reel/2561442584302940/"
+            )
+
+    assert _facebook_resolve_share_id(FakeBot, SHARE_URL) == RESOLVED_ID
+
+
+def test_facebook_share_resolved_id_drives_exact_embed_target():
+    variants = _facebook_embed_urls(SHARE_URL, resolved_id=RESOLVED_ID)
+
+    assert len(variants) == 2
+    assert RESOLVED_ID in variants[1]
