1cd3d3fb519c28d9626d7be0089661c88babb3e7
aliprogram198i <alihumsyali@gmail.com>
2026-09-26 11:25:45 +0300

    test: cover Facebook share/r canonical id resolution

--- tests/test_smart_media_bridge_identity.py
+++ tests/test_smart_media_bridge_identity.py
@@ -20,6 +20,13 @@ class SmartMediaBridgeIdentityTests(unittest.TestCase):
         self.assertIn("1BvGx4dCiQ", variants[0])
         self.assertIn("href=https%3A%2F%2Fwww.facebook.com%2Fshare%2Fr%2F1BvGx4dCiQ%2F", variants[0])
 
+    def test_facebook_share_r_uses_resolved_numeric_id_for_canonical_embed(self):
+        url = "https://www.facebook.com/share/r/1BvGx4dCiQ/"
+        variants = _facebook_embed_urls(url, resolved_id="2561442584302940")
+        self.assertEqual(len(variants), 1)
+        self.assertIn("href=https%3A%2F%2Fwww.facebook.com%2Fwatch%2F%3Fv%3D2561442584302940", variants[0])
+        self.assertNotIn("href=https%3A%2F%2Fwww.facebook.com%2Fshare%2Fr%2F1BvGx4dCiQ%2F", variants[0])
+
     def test_facebook_reel_keeps_numeric_embed_variants(self):
         variants = _facebook_embed_urls("https://www.facebook.com/reel/1474514414731627/")
         self.assertEqual(len(variants), 2)
