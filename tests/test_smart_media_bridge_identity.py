9d63c946d154923b95d651e91d7befd462e9607f
aliprogram198i <alihumsyali@gmail.com>
2026-09-26 10:29:02 +0300

    test: cover Facebook share/r source routing

--- tests/test_smart_media_bridge_identity.py
+++ tests/test_smart_media_bridge_identity.py
@@ -1,6 +1,6 @@
 import unittest
 
-from downloader.smart_media_bridge import _protected_social_post
+from downloader.smart_media_bridge import _facebook_embed_urls, _protected_social_post
 
 
 class SmartMediaBridgeIdentityTests(unittest.TestCase):
@@ -11,6 +11,19 @@ class SmartMediaBridgeIdentityTests(unittest.TestCase):
     def test_instagram_public_post_is_protected(self):
         self.assertTrue(_protected_social_post("https://www.instagram.com/reel/DdYyj56qw54/?stkn=x"))
         self.assertTrue(_protected_social_post("https://www.instagram.com/p/ABC_123/"))
+
+    def test_facebook_share_r_routes_to_official_embed(self):
+        url = "https://www.facebook.com/share/r/1BvGx4dCiQ/"
+        variants = _facebook_embed_urls(url)
+        self.assertEqual(len(variants), 1)
+        self.assertIn("plugins/video.php", variants[0])
+        self.assertIn("1BvGx4dCiQ", variants[0])
+        self.assertIn("href=https%3A%2F%2Fwww.facebook.com%2Fshare%2Fr%2F1BvGx4dCiQ%2F", variants[0])
+
+    def test_facebook_reel_keeps_numeric_embed_variants(self):
+        variants = _facebook_embed_urls("https://www.facebook.com/reel/1474514414731627/")
+        self.assertEqual(len(variants), 2)
+        self.assertTrue(all("plugins/video.php" in variant for variant in variants))
 
     def test_non_social_source_is_not_protected(self):
         self.assertFalse(_protected_social_post("https://example.com/video/123"))
